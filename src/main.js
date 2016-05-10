17680da528c44c91daad468b001d00fb2fa6b8be
banama <306672962@qq.com>
2016-05-10 08:58:42 +0800

    init theme

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import Vdoc from 'vdoc'
-var vdoc = new Vdoc()
-vdoc.mout(require("../vdoc.vdoc"))
-vdoc.theme(require('../theme'))
-vdoc.addRoute('/', require("../theme/root.vue"))
-vdoc.start("#app", {history: false})
-export default vdoc
